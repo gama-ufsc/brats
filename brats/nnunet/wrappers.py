7ec83080d1fa187c9af440eace1294d6eb5f875f
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2021-06-01 11:32:03 -0300

    add env variables check

--- brats/nnunet/wrappers.py
+++ brats/nnunet/wrappers.py
@@ -1,6 +1,18 @@
+import os
 import subprocess
 
 from pathlib import Path
+
+
+# check nnUNet required environment variables
+assert 'nnUNet_raw_data_base' in os.environ, '`nnUNet_raw_data_base` is not set'
+assert Path(os.environ['nnUNet_raw_data_base']).exists(), '`nnUNet_raw_data_base` does not exist'
+
+assert 'nnUNet_preprocessed' in os.environ, '`nnUNet_preprocessed` is not set'
+assert Path(os.environ['nnUNet_preprocessed']).exists(), '`nnUNet_preprocessed` does not exist'
+
+assert 'RESULTS_FOLDER' in os.environ, '`RESULTS_FOLDER` is not set'
+assert Path(os.environ['RESULTS_FOLDER']).exists(), '`RESULTS_FOLDER` does not exist'
 
 
 def predict(input_dir, output_dir, model, fold, task, trainer,
