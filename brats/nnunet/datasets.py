52e7a08ad1e4b4fcf3635b40aecefcbf90a62331
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2021-06-16 15:48:13 -0300

    fix tcga and nnunet dataset creation with all four modalities

--- brats/nnunet/datasets.py
+++ brats/nnunet/datasets.py
@@ -21,10 +21,10 @@ def copy_BraTS_segmentation_and_convert_labels(in_file, out_file):
             raise RuntimeError('unexpected label')
 
     seg_new = np.zeros_like(img_npy)
-    # seg_new[img_npy == 4] = 3
-    # seg_new[img_npy == 2] = 1
-    # seg_new[img_npy == 1] = 2
-    seg_new[img_npy > 0] = 1
+    seg_new[img_npy == 4] = 3
+    seg_new[img_npy == 2] = 1
+    seg_new[img_npy == 1] = 2
+    # seg_new[img_npy > 0] = 1
     img_corr = sitk.GetImageFromArray(seg_new)
     img_corr.CopyInformation(img)
     sitk.WriteImage(img_corr, out_file)
@@ -44,33 +44,27 @@ def make_nnunet_tcga_dataset(dataset_dir, target_images_dir, overwrite):
         assert all([
             flair.exists(),
             t1.exists(),
-            t1c.exists(),
-            t2.exists(),
+            # t1c.exists(),
+            # t2.exists(),
         ]), "%s doesn't have all modalities" % patient_name
 
         new_flair = Path(target_images_dir, patient_name + "_0000.nii.gz")
-        try:
-            new_flair.symlink_to(flair)
-        except FileExistsError:
-            if overwrite:
-                new_flair.unlink()
-                new_flair.symlink_to(flair)
-
         new_t1 = Path(target_images_dir, patient_name + "_0001.nii.gz")
-<<<<<<< Updated upstream
-        try:
-            new_t1.symlink_to(t1)
-        except FileExistsError:
-            if overwrite:
-                new_t1.unlink()
-                new_t1.symlink_to(t1)
-=======
-        new_t1.symlink_to(t1)
         new_t1c = Path(target_images_dir, patient_name + "_0002.nii.gz")
-        new_t1c.symlink_to(t1c)
         new_t2 = Path(target_images_dir, patient_name + "_0003.nii.gz")
-        new_t2.symlink_to(t2)
->>>>>>> Stashed changes
+
+        def _create_or_overwrite(symlink_path, dst_path, overwrite: bool):
+            try:
+                symlink_path.symlink_to(dst_path)
+            except FileExistsError:
+                if overwrite:
+                    symlink_path.unlink()
+                    symlink_path.symlink_to(dst_path)
+
+        _create_or_overwrite(new_flair, flair, overwrite)
+        _create_or_overwrite(new_t1, t1, overwrite)
+        # _create_or_overwrite(new_t1c, t1c, overwrite)
+        # _create_or_overwrite(new_t2, t2, overwrite)
 
     return patient_names
 
@@ -110,18 +104,35 @@ def make_nnunet_dataset(patient_dirs, target_images_dir, target_seg_dir=None):
 
         patient_name = patdir.name
         patient_names.append(patient_name)
-        t1 = Path(patdir, patient_name + "_t1.nii.gz")
         flair = Path(patdir, patient_name + "_flair.nii.gz")
+        t1 = Path(patdir, patient_name + "_t1.nii.gz")
+        t1c = Path(patdir, patient_name + "_t1ce.nii.gz")
+        t2 = Path(patdir, patient_name + "_t2.nii.gz")
 
         assert all([
-            t1.exists(),
             flair.exists(),
-        ]), "%s has no t1+flair" % patient_name
+            t1.exists(),
+            t1c.exists(),
+            t2.exists(),
+        ]), "%s doesn't have all modalities" % patient_name
 
         new_flair = Path(target_images_dir, patient_name + "_0000.nii.gz")
-        new_flair.symlink_to(flair)
         new_t1 = Path(target_images_dir, patient_name + "_0001.nii.gz")
-        new_t1.symlink_to(t1)
+        new_t1c = Path(target_images_dir, patient_name + "_0002.nii.gz")
+        new_t2 = Path(target_images_dir, patient_name + "_0003.nii.gz")
+
+        def _create_or_overwrite(symlink_path, dst_path, overwrite: bool):
+            try:
+                symlink_path.symlink_to(dst_path)
+            except FileExistsError:
+                if overwrite:
+                    symlink_path.unlink()
+                    symlink_path.symlink_to(dst_path)
+
+        _create_or_overwrite(new_flair, flair, True)
+        _create_or_overwrite(new_t1, t1, True)
+        _create_or_overwrite(new_t1c, t1c, True)
+        _create_or_overwrite(new_t2, t2, True)
 
         if target_seg_dir is not None:
             seg = Path(patdir, patient_name + "_seg.nii.gz")
