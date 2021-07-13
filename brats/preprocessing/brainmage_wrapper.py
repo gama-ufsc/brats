6e9fba8e2a21b97931cfd3b9230efaca4e430541
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2021-07-13 09:19:33 -0300

    raise error for unsupported device

--- brats/preprocessing/brainmage_wrapper.py
+++ brats/preprocessing/brainmage_wrapper.py
@@ -10,12 +10,15 @@ def brainmage(in_file_fpath, mask_fpath, device):
 
     if device == 'gpu':
         device = 0
+    elif type(device) != int and device.lower() != 'cpu':
+        raise ValueError("`device` must be 'gpu', 'cpu', or the gpu id (int)")
 
     cmd = f"brain_mage_single_run -i {in_file_fpath} -o {mask_fpath}"\
           f" -dev {device}"
 
     os.environ['MKL_SERVICE_FORCE_INTEL'] = '1'  # to avoid mkl-service error
 
+    print(cmd)
     _ = subprocess.run(cmd, shell=True, check=True)
 
     return mask_fpath
