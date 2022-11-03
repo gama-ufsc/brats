8170a470097638c7b688074c7bdaac76b078f68c
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-11-03 16:47:49 +0000

    remove check for nnunet paths (already done inside nnunet)

--- brats/nnunet/wrappers.py
+++ brats/nnunet/wrappers.py
@@ -4,17 +4,6 @@ import subprocess
 from pathlib import Path
 
 
-# check nnUNet required environment variables
-assert 'nnUNet_raw_data_base' in os.environ, '`nnUNet_raw_data_base` is not set'
-assert Path(os.environ['nnUNet_raw_data_base']).exists(), '`nnUNet_raw_data_base` does not exist'
-
-assert 'nnUNet_preprocessed' in os.environ, '`nnUNet_preprocessed` is not set'
-assert Path(os.environ['nnUNet_preprocessed']).exists(), '`nnUNet_preprocessed` does not exist'
-
-assert 'RESULTS_FOLDER' in os.environ, '`RESULTS_FOLDER` is not set'
-assert Path(os.environ['RESULTS_FOLDER']).exists(), '`RESULTS_FOLDER` does not exist'
-
-
 def predict(input_dir, output_dir, model, fold, task, trainer,
             chk='model_best', overwrite=False):
     input_dir = Path(input_dir)
